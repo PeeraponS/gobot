d34cb6b2474984cdc502b05bd5d3537fab1d1cdc
PeerapornS <peerapornsuk@gosoft.co.th>
2024-03-24 12:26:58 +0700

    add location

---
+++ gobot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gobot
+//
+//  Created by Peerapon Sukprasert on 22/3/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("AT 7-11")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
