4496b0c7a68392702d13e7485a00aa0d6edd834d
PeerapornS <peerapornsuk@gosoft.co.th>
2024-03-24 12:26:58 +0700

    Initial Commit

---
+++ gobot/gobotApp.swift
@@ -0,0 +1,17 @@
+//
+//  gobotApp.swift
+//  gobot
+//
+//  Created by Peerapon Sukprasert on 22/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct gobotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
